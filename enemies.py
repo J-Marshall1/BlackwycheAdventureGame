18d3c91a168e0ab813cf0e04928320cfcbdbd6f2
John Marshall <john.j.marshall@hotmail.com>
2019-03-31 19:45:09 +0100

    created enemy skeleton class

---
+++ enemies.py
@@ -0,0 +1,15 @@
+# john.j.marshall@hotmmail.com
+
+class Enemy:
+  def __init__(self, name, hp, damage):
+    self.name = name
+    self.hp = hp
+    self.damage = damage
+
+  def is_alive(self):
+    return self.hp > 0
+
+class Skeleton(Enemy):
+  def __init__(self):
+    super().__init__(name="Skeleton (Old Pew)", hp=100, damage=15)
+
